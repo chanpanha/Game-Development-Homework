282b4f3b4eb5a4b63969522b396cac036519546b
Chanpanha <panha3311@gmail.com>
2020-01-30 16:05:26 +0700

    Fisrt Commit

---
+++ Assets/Scripts/DefenderSpawner.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DefenderSpawner : MonoBehaviour {
+	
+	Defender defender;
+
+	private void OnMouseDown() {
+		AttemptToPlaceDefenderAt(GetSquareClicked());
+	}
+	
+	public void SetSelectedDefender(Defender defenderToSelect){
+		defender = defenderToSelect;
+	}
+
+	private void AttemptToPlaceDefenderAt( Vector2 gridPos){
+		var StarDisplay = FindObjectOfType<StarDisplay>();
+		int defenderCost = defender.GetStarCost();
+		if (StarDisplay.HaveEnoughStars(defenderCost)){
+			SpawnDefender(gridPos);
+			StarDisplay.SpendStars(defenderCost);
+			
+		
+		}
+		// if we have enough stars
+		// spawn the defenders 
+		// spawn the stars
+	}
+	
+	private Vector2 GetSquareClicked(){
+		Vector2 clickPos = new Vector2 (Input.mousePosition.x, Input.mousePosition.y);
+		Vector2 worldPos = Camera.main.ScreenToWorldPoint(clickPos);
+		Vector2 gridPos = SnapToGrid(worldPos);
+		return	gridPos;
+	}
+
+	
+
+	private Vector2 SnapToGrid(Vector2 rawWorldPos){
+		float newX = Mathf.RoundToInt(rawWorldPos.x);
+		float newY = Mathf.RoundToInt(rawWorldPos.y);
+		return new Vector2(newX, newY);
+	}
+	private void SpawnDefender(Vector2 roundPos){
+		Defender newDefender = Instantiate(defender, roundPos, Quaternion.identity)as Defender;
+		Debug.Log(roundPos);
+	}
+	
+}
---
+++ Assets/Scripts/StarDisplay.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class StarDisplay : MonoBehaviour {
+
+	// Use this for initialization
+
+	[SerializeField] int stars = 100;
+	Text starText;
+	void Start () {
+		starText = GetComponent<Text>();
+		UpdateDisplay(); 
+	}
+	private void UpdateDisplay(){
+		starText.text = stars.ToString();
+	}
+
+	public bool HaveEnoughStars(int amount){
+		return stars >= amount;
+	}
+
+	public void AddStars(int amount){
+		stars += amount;
+		UpdateDisplay();
+	}
+	
+	public void SpendStars(int amount){
+		if (stars >= amount){
+			stars -= amount;
+			UpdateDisplay();
+		}
+	}
+}
